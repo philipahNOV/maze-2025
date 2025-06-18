a3a4c3d7f5cafcc84def935914aa8228d09831a1
Erlend Odd Berge <Erlend.Berge@nov.com>
2025-06-18 12:33:22 +0200

    La til newline på meldingen sendt til arduinoen

--- arduino/jetson_arduino_jetson_latensy_test/jetson_arduino_jetson_latensy_test.py
+++ arduino/jetson_arduino_jetson_latensy_test/jetson_arduino_jetson_latensy_test.py
@@ -2,15 +2,15 @@ import time
 import serial
 import serial.tools.list_ports
 
-print(f'Åpner seriel port\n')
+print(f'Åpner seriel port')
 ser = serial.Serial("/dev/ttyACM0", 9600)
 print(f'Seriel port er åpnet\n')
 
-print(f'Sender melding\n')
+print(f'Sender melding')
 start_time = time.time()  # Ta tidsstempel før sending
 ser.write(b"test_message\n")  # Send melding
 
-print(f'Venter på svar\n')
+print(f'Venter på svar')
 response = ser.readline()  # Les respons
 end_time = time.time()  # Ta tidsstempel ved mottak
 print(f'Svar motat\n')
