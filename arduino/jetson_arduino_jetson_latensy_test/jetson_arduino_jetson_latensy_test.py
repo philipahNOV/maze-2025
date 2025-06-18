87282d93ca42db2da8a39c4aee45f60417722f54
Erlend Odd Berge <Erlend.Berge@nov.com>
2025-06-18 12:22:43 +0200

    La til komentarer

--- arduino/jetson_arduino_jetson_latensy_test/jetson_arduino_jetson_latensy_test.py
+++ arduino/jetson_arduino_jetson_latensy_test/jetson_arduino_jetson_latensy_test.py
@@ -2,13 +2,18 @@ import time
 import serial
 import serial.tools.list_ports
 
+print(f'Åpner seriel port\n')
 ser = serial.Serial("/dev/ttyACM0", 9600)
+print(f'Seriel port er åpnet\n')
 
+print(f'Sender melding\n')
 start_time = time.time()  # Ta tidsstempel før sending
-ser.write(b"test_message")  # Send melding
+ser.write(b"test_message\n")  # Send melding
 
+print(f'Venter på svar\n')
 response = ser.readline()  # Les respons
 end_time = time.time()  # Ta tidsstempel ved mottak
+print(f'Svar motat\n')
 
 total_time = end_time - start_time
 print(f"Tidsforsinkelse: {total_time} sekunder")
