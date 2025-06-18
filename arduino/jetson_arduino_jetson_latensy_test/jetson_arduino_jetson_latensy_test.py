94c9cdb383652d4fb37076d24b99080e61283722
Erlend Odd Berge <Erlend.Berge@nov.com>
2025-06-18 12:11:03 +0200

    Tokk vekk automatisk port finner

--- arduino/jetson_arduino_jetson_latensy_test/jetson_arduino_jetson_latensy_test.py
+++ arduino/jetson_arduino_jetson_latensy_test/jetson_arduino_jetson_latensy_test.py
@@ -2,20 +2,7 @@ import time
 import serial
 import serial.tools.list_ports
 
-def detect_arduino_port():
-        """
-        Detects the port of the Arduino device connected to the system.
-
-        Returns:
-            str: The device port of the Arduino, or None if no Arduino device is found.
-        """
-        ports = list(serial.tools.list_ports.comports())
-        for port in ports:
-            if 'Arduino' in port.description or 'ttyUSB' in port.device or 'ttyACM' in port.device:
-                return port.device
-        return None
-
-ser = serial.Serial(detect_arduino_port(), 9600)
+ser = serial.Serial("/dev/ttyACM0", 9600)
 
 start_time = time.time()  # Ta tidsstempel før sending
 ser.write(b"test_message")  # Send melding
