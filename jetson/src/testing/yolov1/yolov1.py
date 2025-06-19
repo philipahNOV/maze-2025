06caa74539f0e3e72fa7843404c128655ce8c623
philipahNOV <philip.haugen@nov.com>
2025-06-19 09:58:01 +0200

    cam fix

--- jetson/src/testing/yolov1/yolov1.py
+++ jetson/src/testing/yolov1/yolov1.py
@@ -34,7 +34,7 @@ def detect_ball_yolo(model, frame, prev_center=None, max_diff=300):
 
 def main():
     model = YOLO("best.pt")
-    cap = cv2.VideoCapture(2)  # Use webcam; change to 1 or 2 for different camera
+    cap = cv2.VideoCapture(0)  # Use webcam; change to 1 or 2 for different camera
 
     if not cap.isOpened():
         print("Error: Cannot open camera.")
