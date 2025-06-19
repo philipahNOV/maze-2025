45e66a54f256ef87e76125806206859a2fbf4f7c
Lyder <lyder.kjellevold@nov.com>
2025-06-19 08:40:33 +0200

    a

--- jetson/src/horizontal_test.py
+++ jetson/src/horizontal_test.py
@@ -66,7 +66,7 @@ def horizontal(tol = 0.2):
         vel_x = int(kp * abs(theta_x))
         vel_y = int(kp * abs(theta_y))
         print(f"Orientation: {theta_x}, {theta_y} | Velocities: {vel_x}, {vel_y}")
-        arduino_thread.send_target_positions(dir_x, dir_y, vel_x, vel_y)
+        arduino_thread.send_target_positions(dir_x, 2, vel_x, 1)
         time.sleep(3)
     print("Deadline reached, stopping motors.")
 
