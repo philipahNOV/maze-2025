f020fd0abc22b0e4519a79fa30f25ad71e1e964c
Lyder <lyder.kjellevold@nov.com>
2025-06-19 12:46:53 +0200

    a

--- jetson/src/testing/ball_recognition.py
+++ jetson/src/testing/ball_recognition.py
@@ -154,7 +154,8 @@ def detect_red_ball_frame(frame, prev_center=None):
     #cv2.circle(frame, brightest_pixel, 10, (0, 0, 255), 4)
 
     #best_contour = find_most_circular_contour(mask, min_area=1, max_area=800, min_circularity=0.5, prev_center=prev_center, max_diff=10000)
-    M = cv2.moments(masked_frame)
+    gray_mask = cv2.cvtColor(masked_frame, cv2.COLOR_BGR2GRAY)
+    M = cv2.moments(gray_mask)
     if M["m00"] != 0:
         cX = int(M["m10"] / M["m00"])
         cY = int(M["m01"] / M["m00"])
