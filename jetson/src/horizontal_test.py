e2d9d15b1263ef0f38acf3ebc7a4f8496306c40f
Lyder <lyder.kjellevold@nov.com>
2025-06-19 08:27:09 +0200

    a

--- jetson/src/horizontal_test.py
+++ jetson/src/horizontal_test.py
@@ -36,7 +36,7 @@ except Exception as e:
 
 
 def horizontal(tol = 0.2):
-    kp = 500 # Proportional gain for the control loop
+    kp = 1000 # Proportional gain for the control loop
     deadline = time.time() + 20  # 20 seconds deadline
     arduino_thread.send_target_positions(120, 120, 120, 120)  # Stop motors initially
 
@@ -67,7 +67,7 @@ def horizontal(tol = 0.2):
         vel_y = int(kp * abs(theta_y))
         print(f"Orientation: {theta_x}, {theta_y} | Velocities: {vel_x}, {vel_y}")
         arduino_thread.send_target_positions(dir_x, dir_y, vel_x, vel_y)
-        time.sleep(0.3)
+        time.sleep(1)
 
 time.sleep(10)  # Allow time for Arduino connection to stabilize
 horizontal(0.005)
