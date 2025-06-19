aace3a563695b10c16b611809f3bf80d3e408f81
Erlend Odd Berge <Erlend.Berge@nov.com>
2025-06-19 10:23:38 +0200

    La til utregning av posisjnoen fra adc verdiene

--- arduino/actuator_control/actuator_control.ino
+++ arduino/actuator_control/actuator_control.ino
@@ -20,13 +20,27 @@ namespace actuators {
 }
 
 namespace {
-    uint16_t pot_raw_data_act_1{0};
-    uint16_t pot_raw_data_act_2{0};
+    uint16_t pot_raw_data_act_1{analogRead(actuators::actuator_1::pot_feedback)};
+    uint16_t pot_raw_data_act_2{analogRead(actuators::actuator_2::pot_feedback)};
+
+    long dist_act_1{actuator_dist(actuators::actuator_1::pot_feedback)};
+    long dist_act_2{actuator_dist(actuators::actuator_2::pot_feedback)};
 }
+
+
+
+long actuator_dist(uint8_t pot_pin);
+
 
 void setup() {
     // put your setup code here, to run once:
     Serial.begin(9600);
+
+    pot_raw_data_act_1{analogRead(actuators::actuator_1::pot_feedback)};
+    pot_raw_data_act_2{analogRead(actuators::actuator_2::pot_feedback)};
+
+    dist_act_1{actuator_dist(actuators::actuator_1::pot_feedback)};
+    dist_act_2{actuator_dist(actuators::actuator_2::pot_feedback)};
 }
 
 void loop() {
@@ -34,10 +48,31 @@ void loop() {
 
     pot_raw_data_act_1 = analogRead(actuators::actuator_1::pot_feedback);
     pot_raw_data_act_2 = analogRead(actuators::actuator_2::pot_feedback);
+
+    dist_act_1 = actuator_dist(actuators::actuator_1::pot_feedback);
+    dist_act_2 = actuator_dist(actuators::actuator_2::pot_feedback);
     
-    Serial.print("Aktuator 1: ");
+    Serial.print("Aktuator 1: Raw = ");
     Serial.print(pot_raw_data_act_1);
-    Serial.print(" Aktuator 2: ");
-    Serial.println(pot_raw_data_act_2);
+    Serial.print(" Dist = ")
+    Serial.print(dist_act_1)
+
+    Serial.print(" | Aktuator 2: Raw = ");
+    Serial.print(pot_raw_data_act_2);
+    Serial.print(" Dist = ")
+    Serial.print(dist_act_2)
+    Serial.print("\r")
+
     delay(1);
+}
+
+long actuator_dist(uint8_t pot_pin)
+{
+    uint8_t act_max_stroke = 50; // mm
+    uint16_t adc_max_hight = 1018;
+    uint8_t adc_min_hight = 2;
+
+    long dist = map(analogRead(pot_pin), adc_min_hight, adc_max_hight, 0, act_max_stroke);
+
+    return dist;
 }
