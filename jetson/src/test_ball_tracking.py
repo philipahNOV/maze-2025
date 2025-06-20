6c2ec60a1f4e2489d00b5e4f98c13af41f8d3bba
philipahNOV <philip.haugen@nov.com>
2025-06-20 11:17:48 +0200

    dd

--- jetson/src/test_ball_tracking.py
+++ jetson/src/test_ball_tracking.py
@@ -1,6 +1,30 @@
 import testing.yolov1.hsv2 as tracking
 
 ballTracker = tracking.BallTracker("testing/yolov1/best.pt")
-ballTracker.start()
-while True:
+
+try:
+    ballTracker.init_camera()
+    frame = ballTracker.grab_frame()
+    if frame is None:
+        print("Failed to grab frame from camera.")
+        exit(1)
+    
+    ballTracker.start()
+    print("start")
+
+    while True:
+        pos = ballTracker.get_position()
+        if pos is None:
+            print("No position detected.")
+            break
+
+        print(f"Ball position: x={pos[0]}, y={pos[1]}")
+
+except KeyboardInterrupt:
+    print("Keyboard interrupt detected. Stopping the tracker.")
+
     print(ballTracker.get_position())
+
+finally:
+    ballTracker.stop()
+    print("Tracker stopped.")
