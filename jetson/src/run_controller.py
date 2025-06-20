62df62dd5c10022e1cff533238839ebdb12d5e71
Lyder <lyder.kjellevold@nov.com>
2025-06-20 13:50:34 +0200

    a

--- jetson/src/run_controller.py
+++ jetson/src/run_controller.py
@@ -48,6 +48,7 @@ def main():
                 if pos:
                     color = (0, 255, 0) if label == "ball" else (0, 0, 255)
                     cv2.circle(frame, pos, 8, color, -1)
+                    cv2.circle(frame, pos, 5, (0, 0, 255), -1)
                     cv2.putText(frame, label, (pos[0]+10, pos[1]), 
                                 cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
 
