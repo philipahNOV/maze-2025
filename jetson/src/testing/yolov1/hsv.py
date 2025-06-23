8d043c106f5a2b4dc1f0a3bfd377b410b96440be
philipahNOV <philip.haugen@nov.com>
2025-06-23 15:32:41 +0200

    Update HSV ranges for ball detection to improve color identification

--- jetson/src/testing/yolov1/hsv.py
+++ jetson/src/testing/yolov1/hsv.py
@@ -32,9 +32,9 @@ def grab_zed_frame(zed):
 # HSV ranges
 HSV_RANGES = {
     # "ball": (np.array([35, 80, 80]), np.array([85, 255, 255])),      # Green
-    "ball": (np.array([0, 100, 50]), np.array([10, 255, 180])), # Red
-    # "ball": (np.array([0, 0, 90]), np.array([180, 60, 255])), # Grey
-    "marker": (np.array([0, 100, 100]), np.array([10, 255, 255])),   # Red
+    "ball": (np.array([2, 207, 65]), np.array([8, 255, 85])),         # Red (shiny + non-shiny combined)
+    # "ball": (np.array([0, 0, 90]), np.array([180, 60, 255])),       # Grey
+    # "marker": (np.array([0, 100, 100]), np.array([10, 255, 255])),  # Red
 }
 
 tracked_objects = {
