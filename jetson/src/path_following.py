6f4549868a12e1b9d19c1769bdef1f5a860df1ff
Lyder <lyder.kjellevold@nov.com>
2025-06-25 10:07:20 +0200

    Anti stuck path-following

--- jetson/src/path_following.py
+++ jetson/src/path_following.py
@@ -1,5 +1,6 @@
 import positionController
 import numpy as np
+import time
 
 class PathFollower:
     def __init__(self, path_array, controller: positionController.Controller):
@@ -20,6 +21,13 @@ class PathFollower:
 
         self.looping = False
 
+        self.prev_time = None
+        self.prev_ball_pos = None
+
+        self.vel_threshold = 10
+        self.prev_progress_time = None
+        self.stuck_retry_time = 3
+
         if path_array:
             self.path = []
             self.length = len(path_array)
@@ -31,9 +39,30 @@ class PathFollower:
         self.acceptance_radius = self.controller.pos_tol
 
     def follow_path(self, ballPos):
+
+        vel = None
+
+        if self.prev_time and self.prev_ball_pos:
+            dt = time.time() - self.prev_time
+            if dt > 0.000001:
+                vel = np.linalg.norm(np.array(ballPos) - np.array(self.prev_ball_pos)) / dt
+                self.prev_time = time.time()
+                self.prev_ball_pos = ballPos
+
+        if not self.prev_progress_time and vel:
+            if vel < self.vel_threshold:
+                self.prev_progress_time = time.time()
+        elif vel:
+            if vel > self.vel_threshold:
+                self.prev_progress_time = None
+            if time.time() - self.prev_progress_time > self.stuck_retry_time:
+                print(f"Stuck detected. Reversing to waypoint {self.next_waypoint}")
+                self.next_waypoint = max(self.next_waypoint - 1, 0)
+                self.prev_waypoint = max(self.prev_waypoint - 1, 0)
+                self.prev_progress_time = None
+
         
         self.controller.posControl(self.path[self.next_waypoint])
-
 
         if np.linalg.norm(np.array(ballPos) - np.array(self.path[self.next_waypoint])) < self.acceptance_radius:
             self.prev_waypoint = self.next_waypoint
@@ -49,11 +78,12 @@ class PathFollower:
             
             self.next_waypoint = self.next_waypoint + 1
         else:
-            for wpt in reversed(self.path):
+            for i in reversed(range(len(self.path))):
+                wpt = self.path[i]
                 if np.linalg.norm(np.array(ballPos) - np.array(wpt)) < self.acceptance_radius:
 
-                    self.prev_waypoint = self.path.index(wpt)
-                    self.next_waypoint = self.prev_waypoint + 1
+                    self.prev_waypoint = i
+                    self.next_waypoint = i+1
                     return
 
     
