2f041fb6f6884430a9f980df4acd201c8368b7ae
philipahNOV <philip.haugen@nov.com>
2025-06-25 08:40:12 +0200

    Refactor camera offset initialization and enable path looping in PathFollower class

--- jetson/src/path_following.py
+++ jetson/src/path_following.py
@@ -7,21 +7,26 @@ class PathFollower:
         self.path = None
         self.length = 0
 
-        self.camera_offset_x = 420
-        self.camera_offset_y = 10
+        # self.camera_offset_x = 420
+        # self.camera_offset_y = 10
+
+        self.camera_offset_x = 0
+        self.camera_offset_y = 0
 
         self.controller = controller
 
         self.prev_waypoint = None
         self.next_waypoint = 0
 
-        self.looping = False
+        self.looping = True
 
         if path_array:
             self.path = []
             self.length = len(path_array)
             for n in range(self.length):
-                self.path.append((path_array[n][0] + self.camera_offset_x, path_array[n][1] + self.camera_offset_y))
+                # self.path.append((path_array[n][0] + self.camera_offset_x, path_array[n][1] + self.camera_offset_y))
+                self.path.append((path_array[n][0], path_array[n][1]))
+
 
         self.acceptance_radius = self.controller.pos_tol
 
@@ -32,7 +37,7 @@ class PathFollower:
         if np.linalg.norm(np.array(ballPos) - np.array(self.path[self.next_waypoint])) < self.acceptance_radius:
             self.prev_waypoint = self.next_waypoint
 
-            if self.next_waypoint == self.length - 1:
+            if self.next_waypoint == self.length:
                 if self.looping:
                     self.prev_waypoint = None
                     self.next_waypoint = 0
