b639d1334c48fc4a8e58a0e93fdfb63d7e7b3a67
philipahNOV <philip.haugen@nov.com>
2025-06-26 13:03:42 +0200

    Remove obsolete files and code related to horizontal leveling and ball tracking functionality. This includes the deletion of scripts for camera initialization, ball detection, and image capturing, as well as associated image files. The changes streamline the project by eliminating unused components and improving maintainability.

--- jetson/src/camera/board.py
+++ jetson/src/camera/board.py
@@ -1,49 +1,25 @@
+# tested, looks ok, could work, 110 upper bound works on test image
+
 import cv2
 import numpy as np
 
 def get_board(img):
-    """
-    Applies image processing techniques to the input image and returns a masked image.
-
-    Parameters:
-    img (numpy.ndarray): The input image.
-
-    Returns:
-    numpy.ndarray: The masked image.
-
-    """
     width = int(img.shape[1])
     height = int(img.shape[0])
     dim = (width, height)
-
-    # Resize the image
     resized_img = cv2.resize(img, dim, interpolation=cv2.INTER_AREA)
     img_gray = cv2.cvtColor(resized_img, cv2.COLOR_BGR2GRAY)
-
-    # Apply median blur
     img_blurred = cv2.medianBlur(img_gray, 5)
-
-    # Initial bounds
     lower_bound_init = 0
     upper_bound_init = 110
-
-    # Create the mask based on the initial bounds
     mask = np.ones_like(img_blurred) * 255
     mask[(img_blurred >= lower_bound_init) & (img_blurred <= upper_bound_init)] = 0
-
     mask_bgr = cv2.cvtColor(mask, cv2.COLOR_GRAY2BGR)
 
     return mask_bgr
 
 
 def get_mm_per_pixel(img):
-    """
-    Returns the size of the board in the input image.
-
-    Returns:
-    list: The size of the board.
-
-    """
     if img is None:
         return [0, 0]
     board_size = 280 # mm
--- jetson/src/camera/state_variables.py
+++
@@ -1,1 +0,0 @@
-send_images = False
