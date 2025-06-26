e984e31025636fd2feebe84e5210207be98aa7af
philipahNOV <philip.haugen@nov.com>
2025-06-26 08:56:09 +0200

    Update HSV range for ball tracking to improve detection accuracy

--- jetson/src/testing/yolov1/hsv3.py
+++ jetson/src/testing/yolov1/hsv3.py
@@ -21,8 +21,7 @@ class BallTracker:
         }
 
         self.HSV_RANGES = {
-            # "ball": (np.array([35, 80, 80]), np.array([85, 255, 255])), # green
-            "ball": (np.array([37, 33, 34]), np.array([88, 170, 154])),  # dark green
+            "ball": (np.array([35, 80, 80]), np.array([85, 255, 255])), # green
             # "ball": (np.array([0, 100, 50]), np.array([10, 255, 180])), # Red
             "marker": (np.array([0, 100, 100]), np.array([10, 255, 255])),
         }
