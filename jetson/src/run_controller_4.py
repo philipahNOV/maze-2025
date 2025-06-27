f9aaf1204fda8f7190ee15c01fb1e41b7d48ee95
philipahNOV <philip.haugen@nov.com>
2025-06-27 14:40:19 +0200

    Update start and goal coordinates in main function for safe mask visualization

--- jetson/src/run_controller_4.py
+++ jetson/src/run_controller_4.py
@@ -82,13 +82,14 @@ def main(tracker: tracking.BallTracker, controller: positionController_2.Control
     binary_mask = create_binary_mask(gray)
     safe_mask = cv2.dilate(binary_mask, np.ones((3, 3), np.uint8), iterations=2)
 
-    cv2.circle(safe_mask, (738, 699), 5, (0, 255, 0), -1)
+    cv2.circle(safe_mask, (840, 55), 5, (0, 255, 0), -1)
+    cv2.circle(safe_mask, (790, 680), 5, (0, 255, 0), -1)
     cv2.imshow("Safe Mask", safe_mask)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
-    start = (738, 699)
-    goal = (830, 60)
+    start = (840, 55)
+    goal = (790, 680)
 
     path = astar(safe_mask, start, goal, repulsion_weight=5.0)
     waypoints = sample_waypoints(path)
