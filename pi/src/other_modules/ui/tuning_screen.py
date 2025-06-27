ff89c986deb381b5b7ad09f9827bb8c738ac2b6e
Lyder <lyder.kjellevold@nov.com>
2025-06-27 09:01:37 +0200

    a

--- pi/src/other_modules/ui/tuning_screen.py
+++ pi/src/other_modules/ui/tuning_screen.py
@@ -34,7 +34,7 @@ class Tuning(tk.Frame):
             text="↺",  # or "Reset"
             font=("Jockey One", 12),
             width=2,
-            command=lambda: self.load_params(self.params, param_index)
+            command=lambda idx=param_index: self.load_params(self.params, idx)
         )
         reset_btn.grid(row=row, column=col+2, padx=2)
 
