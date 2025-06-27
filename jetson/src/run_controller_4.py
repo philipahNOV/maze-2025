f0f50df883da2056a15bad10c5320cc6180af318
philipahNOV <philip.haugen@nov.com>
2025-06-27 14:57:35 +0200

    dd

--- jetson/src/run_controller_4.py
+++ jetson/src/run_controller_4.py
@@ -80,8 +80,17 @@ def main(tracker: tracking.BallTracker, controller: positionController_2.Control
     binary_mask = create_binary_mask(gray)
     safe_mask = cv2.dilate(binary_mask, np.ones((3, 3), np.uint8), iterations=2)
 
-    start = (55, 840)
-    goal = (680, 790)
+    start = (699, 738)
+    goal = (60, 830)
+
+    # start = (55, 840)
+    # goal = (680, 790)
+
+    cv2.circle(safe_mask, (start[0], start[1]), 15, 127, -1)  # start = (y, x)
+    cv2.circle(safe_mask, (goal[0], goal[1]), 15, 200, -1)    # goal = (y, x)
+    cv2.imshow("Safe Mask", safe_mask)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
     path = astar(safe_mask, start, goal, repulsion_weight=5.0)
     waypoints = sample_waypoints(path)
