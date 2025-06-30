77eb36a010bde0e7386dc1d430a258f8249012cf
Lyder <lyder.kjellevold@nov.com>
2025-06-30 09:34:23 +0200

    Fix stuck detection logic and ensure waypoint navigation boundaries are respected

--- jetson/src/path_following.py
+++ jetson/src/path_following.py
@@ -81,8 +81,8 @@ class PathFollower:
             elif vel > self.vel_threshold:
                 self.prev_progress_time = None
             elif time.time() - self.prev_progress_time > self.stuck_retry_time:
-                print(f"Stuck detected. Reversing to waypoint {self.next_waypoint}")
                 self.next_waypoint = max(self.next_waypoint - 1, 0)
+                print(f"Stuck detected. Reversing to waypoint {self.next_waypoint}")
                 if self.prev_waypoint is None:
                     self.prev_waypoint = max(self.next_waypoint - 1, 0)
                 else:
@@ -103,14 +103,14 @@ class PathFollower:
                     print("Loop completed, starting from first waypoint")
                 print("Done following path")
             
-            self.next_waypoint = self.next_waypoint + 1
+            self.next_waypoint = min(self.next_waypoint + 1, self.length)
         else:
             for i in reversed(range(len(self.path))):
                 wpt = self.path[i]
                 if np.linalg.norm(np.array(ballPos) - np.array(wpt)) < self.acceptance_radius:
 
                     self.prev_waypoint = i
-                    self.next_waypoint = i+1
+                    self.next_waypoint = min(i+1, self.length)
 
         dx = self.path[self.next_waypoint][0] - ballPos[0]
         dy = self.path[self.next_waypoint][1] - ballPos[1]
