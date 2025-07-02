452df422958b8e92af8e0c57d7095d62b8422e79
philipahNOV <philip.haugen@nov.com>
2025-07-02 08:12:40 +0200

    Remove commented-out HSV range for the ball to clean up code

--- jetson/src/testing/yolov1/hsv3.py
+++ jetson/src/testing/yolov1/hsv3.py
@@ -22,7 +22,6 @@ class BallTracker:
 
         self.HSV_RANGES = {
             "ball": (np.array([35, 80, 80]), np.array([85, 255, 255])), # green
-            # "ball": (np.array([0, 0, 180]), np.array([180, 60, 255])), # trying grey
             "marker": (np.array([0, 100, 100]), np.array([10, 255, 255])),
         }
 
