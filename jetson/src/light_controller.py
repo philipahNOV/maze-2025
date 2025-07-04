3b1a42b8846a58c73a3b7f038beeabc805702cbb
Lyder <lyder.kjellevold@nov.com>
2025-07-04 15:33:17 +0200

    a

--- jetson/src/light_controller.py
+++ jetson/src/light_controller.py
@@ -1,7 +1,7 @@
 import time
 
 def blinking_red(arduino_thread):
-    arduino_thread.send_rgb(255, 0, 0)
+    arduino_thread.send_color(255, 0, 0)
     time.sleep(0.5)
-    arduino_thread.send_rgb(0, 0, 0)
-    time.sleep(0.5) 
+    arduino_thread.send_color(0, 0, 0)
+    time.sleep(0.5)
