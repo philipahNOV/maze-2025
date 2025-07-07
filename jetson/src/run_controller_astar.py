11356c297e0acd402355c80a9dcdfd9840791515
philipahNOV <philip.haugen@nov.com>
2025-07-07 09:53:11 +0200

    Refactor main function in run_controller_astar.py: remove unused launch_pad_radius variable and clean up code

--- jetson/src/run_controller_astar.py
+++ jetson/src/run_controller_astar.py
@@ -136,7 +136,7 @@ def draw_path(image, path, waypoints, start, goal):
     return out
 
 def main(tracker: tracking.BallTracker, controller: position_controller.Controller, mqtt_client: MQTTClientJetson):
-    smoother = lowPassFilter.SmoothedTracker(alpha=0.5) #start = (604, 950)
+    smoother = lowPassFilter.SmoothedTracker(alpha=0.5)
     print("Waiting for tracking initialization...")
     while not tracker.initialized:
         time.sleep(0.1)
@@ -155,8 +155,6 @@ def main(tracker: tracking.BallTracker, controller: position_controller.Controll
     gray = get_dynamic_threshold(maze_frame)
     binary_mask = create_binary_mask(gray)
     safe_mask = dilate_mask(binary_mask)
-
-    launch_pad_radius = 70
     
     ball_pos = tracker.get_position()
     while ball_pos is None:
@@ -167,7 +165,6 @@ def main(tracker: tracking.BallTracker, controller: position_controller.Controll
     ball_pos = smoother.update(ball_pos)
     start_raw = (ball_pos[1], ball_pos[0])  # (y, x)
     start = snap_to_nearest_walkable(safe_mask, start_raw)
-    cv2.circle(safe_mask, (ball_pos[1], ball_pos[0]), launch_pad_radius, 255, -1)
 
     #goal = (990, 704)
     global clicked_goal
