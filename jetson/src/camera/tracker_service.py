be979f527ba233cf3093a8ca9c36f14ce1ec74fd
philipahNOV <philip.haugen@nov.com>
2025-07-07 13:01:18 +0200

    Refactor TrackerService to initialize camera in constructor and close it in stop_tracker

--- jetson/src/camera/tracker_service.py
+++ jetson/src/camera/tracker_service.py
@@ -4,12 +4,12 @@ from camera.ball_tracker import BallTracker
 class TrackerService:
     def __init__(self, model_path="best.pt"):
         self.camera = CameraManager()
+        self.camera.init_camera()
         self.tracker = None
         self.started = False
         self.model_path = model_path
 
     def start_tracker(self):
-        self.camera.init_camera()
         self.tracker = BallTracker(self.camera, model_path=self.model_path)
         self.tracker.start()
         self.started = True
@@ -17,8 +17,8 @@ class TrackerService:
     def stop_tracker(self):
         if self.started:
             self.tracker.stop()
-            self.camera.close()
             self.started = False
+            self.camera.close()
 
     def get_ball_position(self):
         return self.tracker.get_position() if self.started else None
