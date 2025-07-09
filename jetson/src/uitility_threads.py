16676f7b8daf6c90cfc0a224f803ceec4aa13626
Lyder <lyder.kjellevold@nov.com>
2025-07-09 16:13:37 +0200

    Update DiscoThread to send iteration index with color command

--- jetson/src/uitility_threads.py
+++ jetson/src/uitility_threads.py
@@ -240,7 +240,7 @@ class DiscoThread(threading.Thread):
                 if i == 5:
                     self.arduino_thread.send_color(255, 0, 0, i)
                 else:
-                    self.arduino_thread.send_color(255, 255, 255)
+                    self.arduino_thread.send_color(255, 255, 255, i)
 
     def stop(self):
         print("[DiscoThread] Stopping disco thread.")
