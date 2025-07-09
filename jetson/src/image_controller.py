b95ccb9a48f0f41d55884d900415f3b9e3759adc
Lyder <lyder.kjellevold@nov.com>
2025-07-09 09:07:42 +0200

    Remove debug print statements from update method in ImageController

--- jetson/src/image_controller.py
+++ jetson/src/image_controller.py
@@ -97,7 +97,6 @@ class ImageController:
         Returns:
             np.ndarray: The final processed frame ready for display.
         """
-        print("TEST1")
         if pathFollower is not None:
             if isinstance(pathFollower, PathFollowerLookahead):
                 self.draw_waypoints_lookahead(pathFollower)
@@ -108,7 +107,6 @@ class ImageController:
                 self.frame = draw_path(self.frame, path, path[0], path[-1])
         self.draw_ball(ballPos)
         self.crop_and_rotate_frame()
-        print("TEST2")
         self.send_frame_to_pi(mqtt_client)
         return self.cropped_frame
     
