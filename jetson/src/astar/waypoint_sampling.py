6ffbb1f42361d22d10342f61eadeb20a7572da8e
philipahNOV <philip.haugen@nov.com>
2025-07-15 13:30:53 +0200

    Update waypoint_spacing and angle_threshold in sample_waypoints function for improved waypoint sampling

--- jetson/src/astar/waypoint_sampling.py
+++ jetson/src/astar/waypoint_sampling.py
@@ -1,7 +1,7 @@
 import math
 from .path_geometry import angle_between, is_clear_path
 
-def sample_waypoints(path, mask, waypoint_spacing=160, angle_threshold=120):
+def sample_waypoints(path, mask, waypoint_spacing=170, angle_threshold=135):
     if not path or len(path) < 2:
         return path or []
 
