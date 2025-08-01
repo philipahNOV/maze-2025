e29a54c04222c375852a36018dc74cf9cc9591f2
Erlend Odd Berge <Erlend.Berge@nov.com>
2025-08-01 13:24:00 +0200

    Add support for building and starting Docker images for Raspberry Pi and Jetson

--- start.sh
+++ start.sh
@@ -1,21 +1,20 @@
 #!/bin/bash
 
-
-
-docker run -it --runtime nvidia --privileged -e DISPLAY  --network host\
-  -v /tmp/.X11-unix:/tmp/.X11-unix \
-  -v /usr/local/zed/resources:/usr/local/zed/resources \
-  -v /dev:/dev \
-  maze-2025-jetson
-
-
-#docker build -t maze2025-app .
-
-#docker run --rm -it \
-#  --runtime=nvidia \
-#  --network host \
-#  --device /dev/video0 \
-#  -v /usr/local/zed:/usr/local/zed \
-#  -v /tmp/argus_socket:/tmp/argus_socket \
-#  -v $(pwd):/app \
-#  maze2025-app
+case "$1" in 
+    --raspberry)
+        echo "Starting docker image for Raspberry pi"
+        docker run -it --privileged -e DISPLAY  --network host \
+        maze-raspberry-image
+        ;;
+    --jetson)
+        echo "Starting docker image for Jetson"
+        docker run -it --runtime nvidia --privileged -e DISPLAY  --network host \
+        -v /tmp/.X11-unix:/tmp/.X11-unix \
+        -v /usr/local/zed/resources:/usr/local/zed/resources \
+        -v /dev:/dev \
+        maze-jetson-image
+        ;;
+    *)
+        echo "Legg til enten --raspberry eller --jetson"
+        ;;
+esac
