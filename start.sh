a86f84e0212fe4986c505652d14a93dfae584fd9
Erlend Odd Berge <Erlend.Berge@nov.com>
2025-08-01 10:16:45 +0200

    Startet på docker image til jetson

--- start.sh
+++ start.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 
-docker build -t maze2025-app .
 
-docker run --rm -it \
-  --runtime=nvidia \
-  --network host \
-  --device /dev/video0 \
-  -v /usr/local/zed:/usr/local/zed \
-  -v /tmp/argus_socket:/tmp/argus_socket \
-  -v $(pwd):/app \
-  maze2025-app
+
+docker run -it --runtime nvidia --privileged -e DISPLAY  --network host\
+  -v /tmp/.X11-unix:/tmp/.X11-unix \
+  -v /usr/local/zed/resources:/usr/local/zed/resources \
+  -v /dev:/dev \
+  maze-2025-jetson
+
+
+#docker build -t maze2025-app .
+
+#docker run --rm -it \
+#  --runtime=nvidia \
+#  --network host \
+#  --device /dev/video0 \
+#  -v /usr/local/zed:/usr/local/zed \
+#  -v /tmp/argus_socket:/tmp/argus_socket \
+#  -v $(pwd):/app \
+#  maze2025-app
