2c501f06f553e6f8941623826cd35a4838839077
philipahNOV <philip.haugen@nov.com>
2025-08-04 09:33:10 +0200

    ss

--- jetson/src/control/astar/board_masking.py
+++ jetson/src/control/astar/board_masking.py
@@ -28,23 +28,4 @@ def create_binary_mask(gray):
     final_mask = cv2.bitwise_and(cleaned, edges_inv)
     final_mask = cv2.morphologyEx(final_mask, cv2.MORPH_CLOSE, np.ones((7, 7), np.uint8), iterations=1)
 
-    # needs to filter green hsv values as white
-    hsv = cv2.cvtColor(cv2.cvtColor(gray, cv2.COLOR_GRAY2BGR), cv2.COLOR_BGR2HSV)
-    lower_green = np.array([35, 20, 20])
-    upper_green = np.array([85, 255, 255])
-    green_mask = cv2.inRange(hsv, lower_green, upper_green)
-    green_mask = cv2.morphologyEx(green_mask, cv2.MORPH_CLOSE, np.ones((7, 7), np.uint8), iterations=2)
-    green_dilated = cv2.dilate(green_mask, np.ones((9, 9), np.uint8), iterations=2)
-
-    flood_fill_mask = green_dilated.copy()
-    contours, _ = cv2.findContours(green_dilated, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    for cnt in contours:
-        M = cv2.moments(cnt)
-        if M["m00"] > 0:
-            cx = int(M["m10"] / M["m00"])
-            cy = int(M["m01"] / M["m00"])
-            cv2.floodFill(flood_fill_mask, None, (cx, cy), 255)
-
-    final_mask = cv2.bitwise_or(final_mask, flood_fill_mask)
-
     return final_mask
