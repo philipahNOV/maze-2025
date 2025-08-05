66add692cd2dee588c74c77c74aa8e9f3f99c311
philipahNOV <philip.haugen@nov.com>
2025-08-05 13:52:52 +0200

    ss

---
+++ jetson/src/joystick2.py
@@ -0,0 +1,71 @@
+import pygame
+import time
+from functools import wraps
+from arduino_connection import ArduinoConnection
+
+
+class JoystickController:
+    def __init__(self, arduino: ArduinoConnection):
+        self.arduino = arduino
+        self.running = False
+        self.deadzone = 5000
+        self.max_raw = 32767
+        self.update_rate_hz = 120
+        self.elevator_state = 1
+        self.prev_button_state = 0
+
+    def _apply_deadzone(func):
+        @wraps(func)
+        def wrapper(self, value):
+            raw = int(value * self.max_raw)
+            if abs(raw) <= self.deadzone:
+                return 0
+            return func(self, raw)
+        return wrapper
+
+    @_apply_deadzone
+    def scaled_output(self, raw):
+        scaled = (abs(raw) - self.deadzone) / (self.max_raw - self.deadzone) * 254 + 1
+        return int(scaled) if raw > 0 else -int(scaled)
+
+    def start(self):
+        pygame.init()
+        pygame.joystick.init()
+
+        try:
+            joystick = pygame.joystick.Joystick(0)
+            joystick.init()
+        except pygame.error:
+            return
+
+        self.running = True
+        interval = 1.0 / self.update_rate_hz
+        print("[XboxController] Started polling at {:.0f} Hz".format(self.update_rate_hz))
+
+        try:
+            while self.running:
+                loop_start = time.time()
+                pygame.event.pump()
+                axis_x = -joystick.get_axis(1)  # venstre horisontal
+                axis_y = -joystick.get_axis(0)  # vensre vertikal
+                vel_x = self.scaled_output(axis_x)
+                vel_y = self.scaled_output(axis_y)
+                self.arduino.send_speed(vel_x, vel_y)
+                button = joystick.get_button(0)
+
+                if button and not self.prev_button_state:
+                    self.elevator_state *= -1
+                    self.arduino.send_elevator(self.elevator_state)
+
+                self.prev_button_state = button
+                time.sleep(max(0, interval - (time.time() - loop_start)))
+
+        except Exception as e:
+            print(f"[XboxController] Exception: {e}")
+
+        finally:
+            self.arduino.send_speed(0, 0)
+            pygame.quit()
+
+    def stop(self):
+        self.running = False
