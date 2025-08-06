a50ce29a47d9a57d993ad3d7aed89f461bbf8a60
Lyder <lyder.kjellevold@nov.com>
2025-08-06 11:33:03 +0200

    a

--- pi/src/other_modules/alive_reciever.py
+++ pi/src/other_modules/alive_reciever.py
@@ -2,18 +2,20 @@ import threading
 import time
 
 class ImAliveThread(threading.Thread):
-    def __init__(self):
+    def __init__(self, app):
         super().__init__(daemon=True)
         self.last_alive_time = None
         self.connection_lost = False
+        self.app = app
 
     def run(self):
         time.sleep(5)  # Initial delay to allow other components to start
         while True:
             elapsed = time.time() - (self.last_alive_time if self.last_alive_time else 0)
-            if elapsed > 5:
+            if elapsed > 5 and not self.connection_lost:
                 self.connection_lost = True
-                print("[ImAliveThread] Connection lost, publishing to MQTT.")
+                self.app.show_frame("InfoScreen")
+                print("[ImAliveThread] Connection lost")
             else:
                 if self.connection_lost:
                     self.connection_lost = False
