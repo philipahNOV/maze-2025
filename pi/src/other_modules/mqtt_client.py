cdf7b09bedd0316634a178a8dd7d053239e92cfa
Lyder <lyder.kjellevold@nov.com>
2025-08-06 14:13:15 +0200

    a

--- pi/src/other_modules/mqtt_client.py
+++ pi/src/other_modules/mqtt_client.py
@@ -153,7 +153,6 @@ class MQTTClientPi(threading.Thread):
                 self.path_failed = True
             if msg.payload.decode() == "alive":
                 if self.alive_reciever is not None:
-                    print("RECIVED ALIVE")
                     self.last_alive_time = time.time()
         elif msg.topic == "pi/tracking_status":
             if self.app and hasattr(self.app, 'frames'):
