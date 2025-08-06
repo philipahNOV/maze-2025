afebb29569a6c85893bdabbc55acb07017a6e484
Lyder <lyder.kjellevold@nov.com>
2025-08-06 11:35:49 +0200

    a

--- pi/src/other_modules/alive_reciever.py
+++ pi/src/other_modules/alive_reciever.py
@@ -9,14 +9,14 @@ class ImAliveThread(threading.Thread):
         self.app = app
 
     def run(self):
-        time.sleep(5)  # Initial delay to allow other components to start
+        time.sleep(12)  # Initial delay to allow other components to start
         while True:
             elapsed = time.time() - (self.last_alive_time if self.last_alive_time else 0)
             if elapsed > 5 and not self.connection_lost:
                 self.connection_lost = True
                 self.app.show_frame("InfoScreen")
                 print("[ImAliveThread] Connection lost")
-            else:
+            elif elapsed <= 5:
                 if self.connection_lost:
                     self.connection_lost = False
             time.sleep(0.3)
