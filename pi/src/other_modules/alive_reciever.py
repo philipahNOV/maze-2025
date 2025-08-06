776fc043606685514fc16755d56facb3a33e9aa8
Lyder <lyder.kjellevold@nov.com>
2025-08-06 14:03:39 +0200

    a

--- pi/src/other_modules/alive_reciever.py
+++ pi/src/other_modules/alive_reciever.py
@@ -12,14 +12,14 @@ class ImAliveThread(threading.Thread):
         time.sleep(5)  # Initial delay to allow other components to start
         while True:
             elapsed = time.time() - (self.app.mqtt_client.last_alive_time if self.app.mqtt_client.last_alive_time else 0)
-            if elapsed > 5 and not self.connection_lost:
+            if elapsed > 12 and not self.connection_lost:
                 self.connection_lost = True
                 self.app.show_frame("ConnectionLostScreen")
                 time.sleep(5)  # Wait before trying to recover
                 print("[ImAliveThread] Connection lost")
                 self.app.mqtt_client.client.publish("jetson/recovery", "recover")
                 self.app.restart_program()
-            elif elapsed <= 5:
+            elif elapsed <= 12:
                 if self.connection_lost:
                     self.connection_lost = False
             time.sleep(0.3)
