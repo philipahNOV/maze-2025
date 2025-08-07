d7b17299a56c924cc550492ffaf60a8cea9ed5f3
pi <lyder.kjellevold@nov.com>
2025-08-07 11:27:01 +0200

    s

--- pi/src/boot_script.sh
+++ pi/src/boot_script.sh
@@ -2,4 +2,7 @@
 
 cd /home/raspberrypi/Documents/maze-2025/pi/src
 
-lxterminal -e "bash -c 'DISPLAY=:0 python3 main.py; exec bash'"
+lxterminal -e "bash -c '\
+  source /home/raspberrypi/.venvs/maze-2025-venv/bin/activate && \
+  DISPLAY=:0 python main.py; \
+  exec bash'"
