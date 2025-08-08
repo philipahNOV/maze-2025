641b0b38ce86d7aa68caa1f036e991d2a225c984
Jetson <philip.haugen@nov.com>
2025-08-08 11:01:43 +0200

    boot

--- jetson/src/boot_script.sh
+++ jetson/src/boot_script.sh
@@ -4,3 +4,5 @@ cd /home/student/Documents/maze-2025/jetson/src
 
 gnome-terminal -- bash -c "/usr/bin/python3 main.py; exec bash"
 gnome-terminal -- bash -c "/usr/bin/python3 recovery_listener.py; exec bash"
+sudo /usr/sbin/nvpmodel -m 0
+sudo /usr/bin/jetson_clocks
