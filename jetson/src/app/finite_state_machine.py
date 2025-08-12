35251f2ec91c872778d90de094f50f045d93c775
Lyder <lyder.kjellevold@nov.com>
2025-08-12 13:54:15 +0200

    a

--- jetson/src/app/finite_state_machine.py
+++ jetson/src/app/finite_state_machine.py
@@ -614,6 +614,7 @@ class HMIController:
             elif cmd == "LoadOffsets":
                 x_offset = self.config["controller"]["arduino"].get("x_offset", 0.002)
                 y_offset = self.config["controller"]["arduino"].get("y_offset", 0.001)
+                offset_file = "offsets.txt"
                 with open(offset_file, "w") as f:
                     f.write(f"{x_offset},{y_offset}")
                 self.mqtt_client.client.publish("pi/command", f"LoadOffsets:{x_offset},{y_offset}")
