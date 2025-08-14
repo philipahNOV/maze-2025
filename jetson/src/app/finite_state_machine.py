32e0cc12134464a89bea39da0cdd2f8ad2dcb81f
Lyder <lyder.kjellevold@nov.com>
2025-08-14 14:32:10 +0200

    Implement cleanup and state management for entering LEADERBOARD mode

--- jetson/src/app/finite_state_machine.py
+++ jetson/src/app/finite_state_machine.py
@@ -970,6 +970,30 @@ class HMIController:
             elif cmd == "Leaderboard":
                 print("[FSM] Entering LEADERBOARD mode")
                 self.state = SystemState.LEADERBOARD
+                self.playalone_timer_start_requested = False
+                self.playalone_game_stop_requested = True
+                
+                if hasattr(self, 'joystick_controller'):
+                    self.joystick_controller.stop()
+                    del self.joystick_controller
+                if hasattr(self, 'joystick_thread') and self.joystick_thread.is_alive():
+                    self.joystick_thread.join()
+                    del self.joystick_thread
+                
+                self.tracking_service.stop_tracker()
+                
+                if self.path_thread is not None and self.path_thread.is_alive():
+                    self.path_thread.stop()
+                    self.path_thread = None
+
+                if self.image_thread is not None:
+                    self.image_thread.stop()
+                    self.image_thread.join()
+                    self.image_thread = None
+                    self.custom_goal = None
+                
+                self.path = None
+                self.image_controller.set_new_path(self.path)
                 self.mqtt_client.client.publish("pi/command", "show_leaderboard_screen")
                 
                 from utils.leaderboard_utils import send_leaderboard_data
